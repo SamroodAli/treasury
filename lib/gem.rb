616e8a6e75478c9023615c997b75718c2a5ad290
samroodAli <devsamrood@gmail.com>
2021-04-02 13:49:28 +0530

    class gem instance variables name, count, description, and version

--- lib/gem.rb
+++ lib/gem.rb
@@ -1,4 +1,10 @@
 class Gem 
   attr reader :name, :version , :description , :downloads
 
+  def initialize(name,version,description,downloads)
+    @name = name
+    @version = version
+    description = description
+    @downloads = downloads
+  end
 end
