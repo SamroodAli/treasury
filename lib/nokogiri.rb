10bc8b799b69901aa5ea9329e290567a4b61ed31
samroodAli <devsamrood@gmail.com>
2021-04-02 13:26:32 +0530

    get version and name in an array

--- lib/nokogiri.rb
+++ lib/nokogiri.rb
@@ -1,12 +1,19 @@
-require "open-uri"
-require "nokogiri"
-doc = Nokogiri::HTML(URI.open("https://rubygems.org/search?query=terminal"))
+require 'open-uri'
+require 'nokogiri'
+doc = Nokogiri::HTML(URI.open('https://rubygems.org/search?query=terminal'))
 
-
-doc.css(".gems__gem").css(".gems__gem__name").each do |query|
+doc.css('.gems__gem').css('.gems__gem__name').each do |query|
   content = query.text
   name = content.split[0]
   version = content.split[1]
-  puts name
-  puts version
+end
+
+doc.css('.gems__gem').each do |query|
+  gems_info = query.css('.gems__gem__info')
+  name_and_version_info = gems_info.css('.gems__gem__name').each do |query|
+    content = query.text
+    name = content.split[0]
+    version = content.split[1]
+    p [name,version]
+  end
 end
