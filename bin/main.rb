e7145901a408635092d7f73aeb2851728a83f31e
samroodAli <devsamrood@gmail.com>
2021-04-02 14:58:01 +0530

    menu body as gem description

--- bin/main.rb
+++ bin/main.rb
@@ -6,7 +6,8 @@ rows = query_results('terminal').map{ |query_items| query_items.values_array}
 
 gem = rows[0]
 header_text = gem.name
-header = {text:header_text}
+header = {text:header_text,color:red}
+body_text = gem.description
 
 table = Terminal::Table.new :rows => rows
 puts table
