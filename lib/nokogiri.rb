8063381695bd7515f3814961e4d4d3dd0a7c74d2
samroodAli <devsamrood@gmail.com>
2021-04-02 12:59:42 +0530

    get version and gem name from the query usign nokogiri

--- lib/nokogiri.rb
+++ lib/nokogiri.rb
@@ -3,6 +3,10 @@ require "nokogiri"
 doc = Nokogiri::HTML(URI.open("https://rubygems.org/search?query=terminal"))
 
 
-doc.css(".gems__gem").css(".gems__gem__name").each do |ele|
-  version = ele.css(".gems__gem__version").text
+doc.css(".gems__gem").css(".gems__gem__name").each do |query|
+  content = query.text
+  name = content.split[0]
+  version = content.split[1]
+  puts name
+  puts version
 end
