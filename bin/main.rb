499d7b6d788618f09bc47127ffcfd98e56b8d9b4
samroodAli <devsamrood@gmail.com>
2021-04-02 15:00:04 +0530

    menu body configurations such as text and choices from body variables

--- bin/main.rb
+++ bin/main.rb
@@ -9,6 +9,7 @@ header_text = gem.name
 header = {text:header_text,color:red}
 body_text = gem.description
 body_choices = ['Install gem','View documentation','go to gem website']
+body = {text:body_text,choices:body_choices,align:"center",color:"white"}
 
 table = Terminal::Table.new :rows => rows
 puts table
