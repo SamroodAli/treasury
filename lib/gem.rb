9cb65e8095a37e24f3de3706aa20585c27f90bc4
samroodAli <devsamrood@gmail.com>
2021-04-02 14:10:00 +0530

    class Gem rename to GemData as it is a reserved keyword in ruby

--- lib/gem.rb
+++ lib/gem.rb
@@ -1,10 +1,10 @@
-class Gem 
-  attr reader :name, :version , :description , :downloads
+class GemData
+  attr_reader :name, :version, :description, :downloads
 
-  def initialize(name,version,description,downloads)
+  def initialize(name, version, description, downloads)
     @name = name
     @version = version
-    description = description
+    @description = description
     @downloads = downloads
   end
 end
--- lib/nokogiri.rb
+++ lib/nokogiri.rb
@@ -1,11 +1,14 @@
 require 'open-uri'
 require 'nokogiri'
-doc = Nokogiri::HTML(URI.open('https://rubygems.org/search?query=music'))
+require_relative './gem'
 
-doc.css('.gems__gem').each do |query|
-  downloads_count = query.css(".gems__gem__downloads__count").text.split[0]
-  gems_info = query.css('.gems__gem__info')
-  description = gems_info.css(".gems__gem__desc").text
-  name,version = gems_info.css('.gems__gem__name').text.split
-  p [name,version,description, downloads_count]
+def query_results
+  doc = Nokogiri::HTML(URI.open('https://rubygems.org/search?query=music'))
+  return doc.css('.gems__gem').map do |query|
+    downloads_count = query.css('.gems__gem__downloads__count').text.split[0]
+    gems_info = query.css('.gems__gem__info')
+    description = gems_info.css('.gems__gem__desc').text
+    name, version = gems_info.css('.gems__gem__name').text.split
+    GemData.new(name, version, description, downloads_count)
+  end
 end
