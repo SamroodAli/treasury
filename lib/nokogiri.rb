6da8e95ef826ad490b9b7f0727bf9881631203a9
samroodAli <devsamrood@gmail.com>
2021-04-02 13:40:12 +0530

    remove nokogiri unused function

--- lib/nokogiri.rb
+++ lib/nokogiri.rb
@@ -1,12 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
-doc = Nokogiri::HTML(URI.open('https://rubygems.org/search?query=terminal'))
-
-doc.css('.gems__gem').css('.gems__gem__name').each do |query|
-  content = query.text
-  name = content.split[0]
-  version = content.split[1]
-end
+doc = Nokogiri::HTML(URI.open('https://rubygems.org/search?query=music'))
 
 doc.css('.gems__gem').each do |query|
   downloads_count = query.css(".gems__gem__downloads__count").text.split[0]
