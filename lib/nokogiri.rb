a715e9240d8f732338fb5d64940e641913a4e096
samroodAli <devsamrood@gmail.com>
2021-04-02 13:28:29 +0530

    get description of gem using nokogiri css selector

--- lib/nokogiri.rb
+++ lib/nokogiri.rb
@@ -10,10 +10,11 @@ end
 
 doc.css('.gems__gem').each do |query|
   gems_info = query.css('.gems__gem__info')
+  description = gems_info.css(".gems__gem__desc").text
   name_and_version_info = gems_info.css('.gems__gem__name').each do |query|
     content = query.text
     name = content.split[0]
     version = content.split[1]
-    p [name,version]
+    p [name,version,description]
   end
 end
