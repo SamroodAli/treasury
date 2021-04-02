3cbd30d2190a695c8c32cab6d7c1b578e1183717
samroodAli <devsamrood@gmail.com>
2021-04-02 14:56:41 +0530

    get one query item for testing

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,10 @@
 require_relative('./../lib/nokogiri')
 require "terminal-table"
-
+require "terminal-basic-menu"
 
 rows = query_results('terminal').map{ |query_items| query_items.values_array}
+
+query_item = rows[0]
 
 table = Terminal::Table.new :rows => rows
 puts table
