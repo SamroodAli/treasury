832fe61fce05b68647c22d65d36697ca8e49b5f0
samroodAli <devsamrood@gmail.com>
2021-04-02 15:36:33 +0530

    menu_array for display array with string interpolation

--- bin/main.rb
+++ bin/main.rb
@@ -3,21 +3,25 @@ require 'terminal-table'
 require 'terminal-basic-menu'
 
 def display_results
-  rows = query_results('terminal').map(&:menu_array)
+  rows = query_results('terminal').map(&:values_array)
   table = Terminal::Table.new rows: rows
   puts table
+  rows
 end
 
 def display_menu(gem)
   header_text = gem[0]
   header = { text: header_text, color: :red }
   body_text = gem[1]
-  body_choices = ['Install gem', 'View documentation', 'go to gem website']
+  body_choices = ['Install this Gem', 'View documentation', 'go to this Gem website']
   body = { text: body_text, choices: body_choices, align: 'center', color: :white }
   footer_text = 'Press q to quit, s to search again'
   footer = { text: footer_text, align: 'center', color: :blue }
   menu1 = Menu.new(header: header, body: body, footer: footer)
   menu1.border_color = :green
-  system('clear')
   menu1.display_menu
 end
+
+rows = query_results('terminal').map(&:menu_array)
+gem = rows[1]
+display_menu(gem)
--- lib/gem.rb
+++ lib/gem.rb
@@ -14,6 +14,7 @@ class GemData
   end
 
   def menu_array
-    [@name, description]
+    body = "#{@description} \n\n version: #{@version} \n\n downloads: #{@downloads}"
+    [@name, body]
   end
 end
