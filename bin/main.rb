24488e632e936da9cc47e52d7fea4f4379eb6e58
samroodAli <devsamrood@gmail.com>
2021-04-03 11:44:44 +0530

    refactor main file to include menu table and menu

--- bin/main.rb
+++ bin/main.rb
@@ -1,18 +1,17 @@
 require_relative('../lib/nokogiri.rb')
-require_relative('./../lib/table')
 require_relative('../lib/system')
 require_relative('../lib/menu')
 
 puts 'hello, what dependency are you looking for ?'
 user_search = gets.chomp
 rows = query_results(user_search)
-display_results(rows)
-
+view = GemMenu.new
+view.display_results rows
   puts 'enter a gem index'
   user_input = gets.chomp.to_i
   rows = rows.map(&:menu_array)
 gem=  rows[user_input]
 
-GemMenu.new(gem)
+view.display_menu gem
 user_input = gets.chomp
 SystemAPI.new gem[0], user_input
