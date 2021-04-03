e988d18fff5e4afd7889196004090ce89755de28
samroodAli <devsamrood@gmail.com>
2021-04-03 12:17:05 +0530

    fix:ri documentation no parameter and prompts while user is waiting

--- lib/system.rb
+++ lib/system.rb
@@ -15,25 +15,30 @@ class SystemAPI
     when '2'
       uninstall_gem(gem)
     when '3'
-      ri_documentation
+      ri_documentation(gem)
     when '4'
       launch_website(gem)
     end
   end
 
   def install_gem(gem)
+    puts "please wait while installing #{gem}"
     system("gem install #{gem}")
   end
 
   def uninstall_gem(gem)
+    puts "please wait while uninstalling #{gem}"
     system("gem uninstall #{gem}")
   end
 
-  def ri_documentation
-    system("ri #{header_text}")
+  def ri_documentation(gem)
+    puts "please wait while loading documentation for #{gem}"
+    system("ri #{gem}")
   end
 
   def launch_website(gem)
+    puts "please wait while launching #{gem} online documentation"
+    puts 'press CONTROL and C together to exit session'
     uri = RUBY_WEBSITE + gem
 
     Launchy.open(uri) do |exception|
