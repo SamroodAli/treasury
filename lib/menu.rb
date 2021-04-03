c88433ca55f51992280945a0851d8eb5b155afe0
samroodAli <devsamrood@gmail.com>
2021-04-03 10:15:40 +0530

    menu:footer config for menu refactor to seperate method

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,8 +3,7 @@ require_relative('./system')
 require_relative('./CONSTANTS.rb')
 
 def display_menu(gem)
-  footer_text = 'Press q to quit, s to search again'
-  footer = { text: footer_text, align: 'center', color: :blue }
+  
   menu1 = Menu.new(header: header, body: body, footer: footer)
   menu1.border_color = :green
   menu1.display_menu
@@ -17,9 +16,15 @@ def header_config(gem)
   header
 end
 
-def menu_body_config(gem)
+def body_config(gem)
   body_text = gem[1]
   body_choices = MENU
   body = { text: body_text, choices: body_choices, align: 'center', color: :white }
   body
+end
+
+def footer_config(gem)
+  footer_text = 'Press q to quit, s to search again'
+  footer = { text: footer_text, align: 'center', color: :blue }
+  footer
 end
