134e26fe3c2b62ba7b7bc3d1f41b05c8b42dd641
samroodAli <devsamrood@gmail.com>
2021-04-03 10:58:56 +0530

    refactor system api to class SystemAPI

--- lib/system.rb
+++ lib/system.rb
@@ -1,39 +1,47 @@
 require 'launchy'
 require_relative('./constants')
 
-def system_interface(header_text)
-  user_input = gets.chomp
-  gem_interface(header_text, user_input)
-end
+class SystemAPI
+  def system_interface(header_text)
+    user_input = gets.chomp
+    gem_interface(header_text, user_input)
+  end  
+  
+  private 
 
-def gem_interface(gem, user_input)
-  case user_input
-  when '1'
-    install_gem(gem)
-  when '2'
-    uninstall_gem(gem)
-  when '3'
-    system("ri #{header_text}")
-  when '4'
-    launch_website(gem)
+  def gem_interface(gem, user_input)
+    case user_input
+    when '1'
+      install_gem(gem)
+    when '2'
+      uninstall_gem(gem)
+    when '3'
+      ri_documentation
+    when '4'
+      launch_website(gem)
+    end
   end
-end
+  
+  def install_gem(gem)
+    puts gem
+    system("gem install #{gem}")
+  end
+  
+  def uninstall_gem(gem)
+    system("gem uninstall #{gem}")
+  end
+  
+  def ri_documentation
+    system("ri #{header_text}")
+  end
+  
+  def launch_website(gem)
+    uri = RUBY_WEBSITE + gem
 
-def install_gem(gem)
-  puts gem
-  system("gem install #{gem}")
-end
-
-def uninstall_gem(gem)
-  system("gem uninstall #{gem}")
-end
-
-def launch_website(gem)
-  uri = RUBY_WEBSITE + gem
-
-  Launchy.open(uri) do |exception|
-    puts "Attempted to open #{uri} and failed because #{exception}"
-    system('exit')
+    Launchy.open(uri) do |exception|
+      puts "Attempted to open #{uri} and failed because #{exception}"
+      system('exit')
+    end
   end
 end
 
