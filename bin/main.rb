92d04cce0ad876c3da18ec4d71b3dfe4a940f6d8
samroodAli <devsamrood@gmail.com>
2021-04-03 13:30:29 +0530

    wait prompt for user while fetching gems

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,6 @@ require_relative('../lib/model')
 require_relative('../lib/system')
 require_relative('../lib/view')
 
-
 def user_input
   gets.chomp
 end
@@ -11,14 +10,11 @@ def user_num
   gets.chomp.to_i
 end
 
-model =Model.new
-
 puts 'hello, what dependency are you looking for ?'
-results = model.query_results(user_input)
+model = Model.new user_input
 view = View.new
-view.table results
+view.table model.results
 puts 'enter a gem index'
-gem = results[user_num].menu_array
+gem = model.results[user_num].menu_array
 view.menu gem
 SystemAPI.new gem[0], user_input
-
--- lib/model.rb
+++ lib/model.rb
@@ -3,18 +3,25 @@ require 'nokogiri'
 require_relative './gem'
 
 class Model
+  attr_reader :results
 
-def query_results(search)
-  doc = Nokogiri::HTML(URI.open("https://rubygems.org/search?query=#{search}"))
-  doc.css('.gems__gem').map.with_index { |query, index| GemData.new(*query_data(query, index)) }
-end
+  def initialize(user_input)
+    @results = query_results(user_input)
+  end
 
-def query_data(query, index)
-  gems_info = query.css('.gems__gem__info')
-  description = gems_info.css('.gems__gem__desc').text
-  name, version = gems_info.css('.gems__gem__name').text.split
-  downloads_count = query.css('.gems__gem__downloads__count').text.split[0]
-  [index, name, version, description, downloads_count]
-end
+  private
 
+  def query_results(search)
+    puts "please wait while fetching gems"
+    doc = Nokogiri::HTML(URI.open("https://rubygems.org/search?query=#{search}"))
+    doc.css('.gems__gem').map.with_index { |query, index| GemData.new(*query_data(query, index)) }
+  end
+
+  def query_data(query, index)
+    gems_info = query.css('.gems__gem__info')
+    description = gems_info.css('.gems__gem__desc').text
+    name, version = gems_info.css('.gems__gem__name').text.split
+    downloads_count = query.css('.gems__gem__downloads__count').text.split[0]
+    [index, name, version, description, downloads_count]
+  end
 end
