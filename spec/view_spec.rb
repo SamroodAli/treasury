d043de75defe211b1f3b096b1c84b5195b84e153
samroodAli <devsamrood@gmail.com>
2021-04-05 15:58:13 +0530

    remove name attribute for gem mock double

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/view'
 
 describe 'View' do
   subject(:view) { View.new }
-  let(:gem) { double('gem', menu_data: 'this is some data for testing',name=>'colorize') }
+  let(:gem) { double('gem', menu_data: 'this is some data for testing') }
   let(:model) { double('model', results: [gem, gem, gem]) }
   describe '#menu' do
     it 'should not raise issue for gem terminal-basic-menu' do
