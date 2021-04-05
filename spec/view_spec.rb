e0e55dea002a52ee27b630c49aee51c24b592e2c
samroodAli <devsamrood@gmail.com>
2021-04-05 16:05:23 +0530

    Fix:missing > in mock double gem

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/view'
 
 describe 'View' do
   subject(:view) { View.new }
-  let(:gem) { double('gem', :menu_data: 'this is some data for testing',:name=>'colorize', :table_row_format=['0','colorize','0.1','50000']) }
+  let(:gem) { double('gem', :menu_data: 'this is some data for testing',:name=>'colorize', :table_row_format=>['0','colorize','0.1','50000']) }
   let(:model) { double('model', results: [gem, gem, gem]) }
   describe '#menu' do
     it 'should not raise issue for gem terminal-basic-menu' do
