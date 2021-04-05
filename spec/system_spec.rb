a1808e1cf0dcfc35b924436439f72a7b18b16099
samroodAli <devsamrood@gmail.com>
2021-04-05 15:32:30 +0530

    Fix:system api take two arguments error

--- spec/system_spec.rb
+++ spec/system_spec.rb
@@ -1,4 +1,9 @@
 require 'rspec'
 
 describe 'System' do
+  describe '#initialization' do
+    it 'should take in two arguments' do
+      expect {SystemAPI.new('colorize','1')}.not_to raise_error
+    end
+  end
 end
