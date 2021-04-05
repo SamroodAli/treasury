97af91f096cdc206c5bb53901ad7700b077d4631
samroodAli <devsamrood@gmail.com>
2021-04-05 15:52:57 +0530

    Test:It block for describe #table method

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -6,11 +6,13 @@ describe 'View' do
   let(:gem) { double('gem', menu_data: 'this is some data for testing') }
   let(:model) { double('model', results: [gem, gem, gem]) }
   describe '#menu' do
-    it 'should call display play with menu config(gem)' do
-      expect { view.menu(gem) }.not_to raise_error
+    it 'should not raise issue for gem terminal-basic-menu' do
+      expect{ view.menu(gem) }.not_to raise_error
     end
   end
   describe '#table' do
-    expect { view.table(gem.results) }.not_to raise_error
+    it 'should not raise issue for temrinal table gem' do
+    expect{ view.table(gem.results) }.not_to raise_error
+    end
   end
 end
