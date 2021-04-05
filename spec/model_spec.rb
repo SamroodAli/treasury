3c09842cf3200bc995c33d5835db37c6f9481109
samroodAli <devsamrood@gmail.com>
2021-04-05 15:02:42 +0530

    Test:context for last gem

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -1,8 +1,8 @@
 require 'rspec'
-require_relative '../lib/model.rb'
+require_relative '../lib/model'
 
 describe 'Model' do
-  subject(:model){ Model.new("terminal") }
+  subject(:model) { Model.new('terminal') }
   describe '#initialize' do
     it 'should set @gems to an array' do
       expect(subject.gems).to be_an(Array)
@@ -16,28 +16,35 @@ describe 'Model' do
     end
 
     it 'should set @current_gem as as @gems[index]' do
-    model.gem(10)
-    expect(subject.current_gem).to eq(model.gems[10]) 
+      model.gem(10)
+      expect(subject.current_gem).to eq(model.gems[10])
     end
 
-    it'should return current gem from the gems list' do
+    it 'should return current gem from the gems list' do
       gem = model.gem(10)
       expect(gem).to eq(model.gems[10])
     end
   end
 
   describe 'next_gem' do
-    'it should change index to index + 1' do
+    it 'should change index to index + 1' do
       model.next_gem
       expect(model.index).to eq(1)
     end
 
-    'it should not set index  + 1 when at last gem' do
-      last_gem = model.size - 1
-      model.gem(last_gem)
-      expect(model.index).not_to eq(last_gem + 1)
+    context 'When at last gem' do
+      before(:each) do
+        last_gem = model.size - 1
+        model.gem(last_gem)
+      end
+
+      it 'should not set index  + 1 when at last gem' do
+        expect(model.index).not_to eq(last_gem + 1)
+      end
+
+      it 'should set to zero for last gem' do
+        expect(model.index).to be_zero
+      end
     end
-
-
   end
 end
