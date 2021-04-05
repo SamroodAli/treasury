9ddeea50ffff7663ff842a00d2d5fa7ca1488dcb
samroodAli <devsamrood@gmail.com>
2021-04-05 16:24:11 +0530

    Add table row format for mock double gem

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/view'
 
 describe 'View' do
   subject(:view) { View.new }
-  let(:gem) { double('gem','name'=>'colorize','description'=>'some description','version'=>'1','downloads'=>'4444','index'=>'20') }
+  let(:gem) { double('gem','name'=>'colorize','description'=>'some description','version'=>'1','downloads'=>'4444','index'=>'20','table_row_format'=>['0','colorize','1','4444']) }
   let(:model) { double('model', results: [gem, gem, gem]) }
   describe '#menu' do
     it 'should not raise issue for gem terminal-basic-menu' do
