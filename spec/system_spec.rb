a6bc7f45c0148140cf58b282118b9c453f5e1600
samroodAli <devsamrood@gmail.com>
2021-04-05 15:32:51 +0530

    Fix:rubocop offenses

--- spec/system_spec.rb
+++ spec/system_spec.rb
@@ -3,7 +3,7 @@ require 'rspec'
 describe 'System' do
   describe '#initialization' do
     it 'should take in two arguments' do
-      expect {SystemAPI.new('colorize','1')}.not_to raise_error
+      expect { SystemAPI.new('colorize', '1') }.not_to raise_error
     end
   end
 end
