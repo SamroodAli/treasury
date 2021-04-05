5393e7d239cb19df92d61c7629f23e8b8ff517cd
samroodAli <devsamrood@gmail.com>
2021-04-05 15:15:33 +0530

    Add model.next gem in test in rspec

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -39,12 +39,14 @@ describe 'Model' do
       it 'should not set index  + 1 when at last gem' do
         last_gem = model.size - 1
         model.gem(last_gem)
+        model.next_gem
         expect(model.index).not_to eq(last_gem + 1)
       end
 
       it 'should set to zero for last gem' do
         last_gem = model.size - 1
         model.gem(last_gem)
+        model.next_gem
         expect(model.index).to be_zero
       end
     end
