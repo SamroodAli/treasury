6c2791d5a81591dcff59f1cef6ea46353255b4c3
samroodAli <devsamrood@gmail.com>
2021-04-05 16:13:07 +0530

    Remove table row format from double gem

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,10 +3,11 @@ require_relative '../lib/view'
 
 describe 'View' do
   subject(:view) { View.new }
-  let(:gem) { double('gem', :menu_data: 'this is some data for testing',:name=>'colorize', :table_row_format=>(['0','colorize','0.1','50000'])) }
+  let(:gem) { double('gem','name'=>'colorize') }
   let(:model) { double('model', results: [gem, gem, gem]) }
   describe '#menu' do
     it 'should not raise issue for gem terminal-basic-menu' do
+      gem.name='colorize'
       expect { view.menu(gem) }.not_to raise_error
     end
   end
