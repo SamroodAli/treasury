52823d40dd08a95a78567e20f9dd667e123808cc
samroodAli <devsamrood@gmail.com>
2021-04-05 16:19:41 +0530

    Add attributes for double gem in view

--- spec/view_spec.rb
+++ spec/view_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/view'
 
 describe 'View' do
   subject(:view) { View.new }
-  let(:gem) { double('gem','name'=>'colorize') }
+  let(:gem) { double('gem','name'=>'colorize','description'=>'some description','version'=>'1','downloads'=>'4444','index'=>'20') }
   let(:model) { double('model', results: [gem, gem, gem]) }
   describe '#menu' do
     it 'should not raise issue for gem terminal-basic-menu' do
