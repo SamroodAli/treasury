7f796f64bfab793f798e499b6abd4cb6c7946124
samroodAli <devsamrood@gmail.com>
2021-04-05 15:48:16 +0530

    Test:view class menu and table methods gem wrappers test

---
+++ spec/view_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require_relative '../lib/view'
+
+describe 'View' do
+  subject(:view) { View.new }
+  let(gem) { double('gem', menu_data: 'this is some data for testing') }
+  let(model) { double('model', results: [gem, gem, gem]) }
+  descibe '#menu' do
+    it 'should call display play with menu config(gem)' do
+      expect { view.menu(gem) }.not_to raise_error
+    end
+  end
+  describe '#table' do
+    expect { view.table(gem.results) }.not_to raise_error
+  end
+end
